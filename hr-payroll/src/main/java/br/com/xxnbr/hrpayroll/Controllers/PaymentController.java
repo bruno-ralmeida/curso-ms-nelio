bf8e26530b54d5a3579595cddd138c7456c8088e
Bruno Almeida <bruno-ralmeida@outlook.com>
2023-01-08 15:09:12 -0300

    :truck: chore: Pasta controller renomeada

--- hr-payroll/src/main/java/br/com/xxnbr/hrpayroll/Controllers/PaymentController.java
+++ hr-payroll/src/main/java/br/com/xxnbr/hrpayroll/Controllers/PaymentController.java
@@ -1,4 +1,4 @@
-package br.com.xxnbr.hrpayroll.Controllers;
+package br.com.xxnbr.hrpayroll.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
