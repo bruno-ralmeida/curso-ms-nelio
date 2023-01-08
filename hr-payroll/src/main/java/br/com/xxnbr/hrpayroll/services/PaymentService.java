9faab9cc177ceef48360343bc840a68fc0f79787
Bruno Almeida <bruno-ralmeida@outlook.com>
2023-01-08 17:17:33 -0300

    :sparkles: feat: Implementação do openfeign para chamadas

--- hr-payroll/src/main/java/br/com/xxnbr/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/xxnbr/hrpayroll/services/PaymentService.java
@@ -1,36 +1,27 @@
 package br.com.xxnbr.hrpayroll.services;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.xxnbr.hrpayroll.entitites.Payment;
-import br.com.xxnbr.hrpayroll.entitites.Worker;
 
 @Service
 public class PaymentService {
 
-  @Value("${hr-worker.host}")
-  private String host;
-
   @Autowired
-  private RestTemplate restTemplate;
+  private WorkerFeignClient feignClient;
 
   public Payment getPayment(long workerId, int days) {
-    var url = host.concat("/workers/{id}");
-
-    var worker = restTemplate.getForObject(url, Worker.class, workerId);
+    var worker = feignClient.findById(workerId).getBody();
 
     if (null == worker)
-      throw new IllegalArgumentException("Worker id invalid!");
+      throw new IllegalArgumentException("Worker ID invalid!");
 
     return Payment.builder()
         .name(worker.getName())
         .dailyIncome(worker.getDailyIncome())
         .days(days)
         .build();
+
   }
 }
---
+++ hr-payroll/src/main/java/br/com/xxnbr/hrpayroll/services/WorkerFeignClient.java
@@ -0,0 +1,18 @@
+package br.com.xxnbr.hrpayroll.services;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.com.xxnbr.hrpayroll.entitites.Worker;
+
+@Component
+@FeignClient(name = "hr-worker", url = "http://localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<Worker> findById(@PathVariable Long id);
+
+}
